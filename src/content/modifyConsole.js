a5c847f472e8ba567ebdae721a07c23f45c774fd
Niklas Entschladen <niklasent@users.noreply.github.com>
2023-02-12 17:40:44 +0100

    Implement LogGet detection.

--- src/content/modifyConsole.js
+++ src/content/modifyConsole.js
@@ -1,8 +1,9 @@
 (() => {
+    // Modify console.clear function for ConClear detection.
     const CONSOLE_CLEAR_TIME_THRESHOLD_MS = 2000;
     var lastCallTime = undefined;
 
-    console.clear = (function(old_console_clear) {
+    console.clear = (function(originalConsoleClear) {
         function extendsConsoleClear() {
             var thisCallTime = performance.now();
             if (lastCallTime) {
@@ -14,9 +15,42 @@
                     window.postMessage(msg);
                 }
             }
-            old_console_clear();
+            originalConsoleClear();
             lastCallTime = thisCallTime;
         }
         return extendsConsoleClear;
     })(console.clear);
+
+    // Modify console functions for LogGet detection.
+    const EXCLUDED_FUNCTIONS = ["clear", "count", "countReset", "group", "groupCollapsed", "groupEnd", "time", "timeEnd"];
+
+    for (let key in console) {
+        if (typeof console[key] === "function" && !(EXCLUDED_FUNCTIONS.includes(key))) {
+            console.log(key);
+            var originalConsoleFunction = console[key];
+            console[key] = function(...args) {
+                args.forEach((argument) => {
+                    if (argument !== undefined) {
+                        if (argument.toString !== undefined) {
+                            if (argument.hasOwnProperty("toString")) {
+                                var msg = {
+                                    req: "ADT",
+                                    data: ["logget"]
+                                };
+                                window.postMessage(msg);
+                            }
+                        }
+                        if (typeof argument.id === "function") {
+                            var msg = {
+                                req: "ADT",
+                                data: ["logget"]
+                            };
+                            window.postMessage(msg);
+                        }
+                    }
+                });
+                originalConsoleFunction.apply(console, args);
+            }
+        };
+    };
 })();
