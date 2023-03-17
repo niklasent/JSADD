35f96dee7225eed816b23c49aac58f744c0cdfbe
Niklas Entschladen <niklasent@users.noreply.github.com>
2023-03-17 10:59:39 +0100

    Avoid storage error in popup.

--- platforms/chrome/src/popup/popup.js
+++ platforms/chrome/src/popup/popup.js
@@ -43,40 +43,42 @@ document.addEventListener("DOMContentLoaded", async () => {
     const storage = chrome.storage.local;
     // const storage = chrome.storage.sync;
     storage.get([webURL + "_ADT"], (data) => {
-        for (let i = 0; i < data[webURL + "_ADT"].length; i++) {
-            let adt = antiDebuggingTechniques[data[webURL + "_ADT"][i]];
-            if (!((data[webURL + "_ADT"][i] === "trigbreak") && debuggerDisabled)) {
-                // Mark categories that contain an entry.
-                let categorySummaryElement = document.getElementById(adt.type + "-sum");
-                categorySummaryElement.style.backgroundColor = "red";
-                categorySummaryElement.style.color = "white";
-                let categoryCountElement = document.getElementById(adt.type + "-count");
-                categoryCountElement.style.backgroundColor = "red";
-                let categoryDescElement = document.getElementById(adt.type + "-desc");
-                categoryDescElement.style.backgroundColor = "red";
-                // Show total number of entries per category.
-                let categoryCountString = categoryCountElement.innerText;
-                let categoryCount = parseInt(categoryCountString.substring(0, categoryCountString.indexOf('/'))) + 1;
-                categoryCountString = categoryCount.toString() + categoryCountString.substring(categoryCountString.indexOf('/'))
-                categoryCountElement.innerText = categoryCountString;
-                // Add entries to categories.
-                let categoryList = document.getElementById(adt.type + "-list");
-                let listElement = document.createElement('li');
-                let headline = document.createElement('h1');
-                headline.innerText = adt.name;
-                let description = document.createElement('p')
-                description.innerText = adt.desc;
-                let author = document.createElement('p');
-                author.innerText = adt.auth + ", " + adt.year;
-                author.style.fontStyle = 'italic';
-                let referenceLink = document.createElement('a');
-                referenceLink.href = adt.link;
-                referenceLink.textContent = "[Learn more]";
-                listElement.appendChild(headline);
-                listElement.appendChild(description);
-                listElement.appendChild(author);
-                listElement.appendChild(referenceLink);
-                categoryList.appendChild(listElement);
+        if (data[webURL + "_ADT"]) {
+            for (let i = 0; i < data[webURL + "_ADT"].length; i++) {
+                let adt = antiDebuggingTechniques[data[webURL + "_ADT"][i]];
+                if (!((data[webURL + "_ADT"][i] === "trigbreak") && debuggerDisabled)) {
+                    // Mark categories that contain an entry.
+                    let categorySummaryElement = document.getElementById(adt.type + "-sum");
+                    categorySummaryElement.style.backgroundColor = "red";
+                    categorySummaryElement.style.color = "white";
+                    let categoryCountElement = document.getElementById(adt.type + "-count");
+                    categoryCountElement.style.backgroundColor = "red";
+                    let categoryDescElement = document.getElementById(adt.type + "-desc");
+                    categoryDescElement.style.backgroundColor = "red";
+                    // Show total number of entries per category.
+                    let categoryCountString = categoryCountElement.innerText;
+                    let categoryCount = parseInt(categoryCountString.substring(0, categoryCountString.indexOf('/'))) + 1;
+                    categoryCountString = categoryCount.toString() + categoryCountString.substring(categoryCountString.indexOf('/'))
+                    categoryCountElement.innerText = categoryCountString;
+                    // Add entries to categories.
+                    let categoryList = document.getElementById(adt.type + "-list");
+                    let listElement = document.createElement('li');
+                    let headline = document.createElement('h1');
+                    headline.innerText = adt.name;
+                    let description = document.createElement('p')
+                    description.innerText = adt.desc;
+                    let author = document.createElement('p');
+                    author.innerText = adt.auth + ", " + adt.year;
+                    author.style.fontStyle = 'italic';
+                    let referenceLink = document.createElement('a');
+                    referenceLink.href = adt.link;
+                    referenceLink.textContent = "[Learn more]";
+                    listElement.appendChild(headline);
+                    listElement.appendChild(description);
+                    listElement.appendChild(author);
+                    listElement.appendChild(referenceLink);
+                    categoryList.appendChild(listElement);
+                }
             }
         }
     });
